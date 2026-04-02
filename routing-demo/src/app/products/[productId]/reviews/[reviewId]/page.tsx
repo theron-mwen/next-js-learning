9c9bdc6f5d8162153a0a0420338071fc362d7e79
Theron <theron-mwen>
2026-04-02 22:59:40 +0800

    Tuts 25 - Error Handling

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,17 @@
 import { notFound, redirect } from "next/navigation";
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  const random = getRandomInt(2);
+  if (random ===1){
+    throw new Error("Error loading review");
+  }
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000 || !productId || !reviewId) {
     // notFound();
