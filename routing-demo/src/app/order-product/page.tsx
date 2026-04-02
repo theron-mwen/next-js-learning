d7d2980a5a3cb9909648913c5f062742d01e98cc
Theron <theron-mwen>
2026-04-02 22:43:41 +0800

    Tuts 22 - Navigating Programmatically

---
+++ routing-demo/src/app/order-product/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function OrderProduct() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/");
+    // router.replace("/");
+    // router.back();
+    // router.forward();
+  };
+
+  return (
+    <>
+      <h1>Order Product</h1>
+      <button onClick={handleClick}>Place order</button>
+    </>
+  );
+}
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 export default async function ReviewDetails({
   params,
 }: {
@@ -6,7 +6,8 @@ export default async function ReviewDetails({
 }) {
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000 || !productId || !reviewId) {
-    notFound();
+    // notFound();
+    redirect("/products");
   }
   return (
     <div>
