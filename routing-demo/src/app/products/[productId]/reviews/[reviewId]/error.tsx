aaa3ae63da17c8c5947918b0f4fc85901d8ae2f2
Theron <theron-mwen>
2026-04-02 23:08:03 +0800

    Tuts 27 - Handling Errors in Nested Routes

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -22,3 +22,12 @@ export default function ErrorBoundary({
     </div>
   );
 }
+// Error always bubble up to find th closes parent error boundary
+// An error.tsx file handdles errors not just for its own folder, but for all the nested child
+// segment below it to
+
+// By strategically placing error.tsx files at different levels in your route folder, you
+// can control exactly how detailed you error handling gets
+
+// Where you put your error.tsx file makes a huge difference - it determines exactly
+// which parts of your UI get affected when things go wrong.
