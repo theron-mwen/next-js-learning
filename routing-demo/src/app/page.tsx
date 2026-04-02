3763405fea5cf8f386f69deb7552f9875b3929e5
Theron <theron-mwen>
2026-04-02 22:15:19 +0800

    Tuts 19 - Link Component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <h1>Welcome Home!</h1>
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <nav>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Products</Link>
+      </nav>
+    </>
+  );
 }
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link"
+
+
 export default function ProductList() {
+    const productId = 100
     return (
         <>
+            <Link href="/">Home</Link>
             <h1>Product List</h1>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                <li><Link href="/products/product-1">Product 1</Link></li>
+                <li><Link href="/products/product-2" replace>Product 2</Link></li>
+                <li><Link href={`/products/${productId}`}>Product ${productId}</Link></li>
             </ul>
         </>
     )
