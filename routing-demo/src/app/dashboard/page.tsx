f620ac47e7989297762766737fffecdaa4851091
Theron <theron-mwen>
2026-04-02 21:17:20 +0800

    Tuts 11: File colocation

---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+function Barchart() {
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Barchart />
+        </div>
+    )
+}
