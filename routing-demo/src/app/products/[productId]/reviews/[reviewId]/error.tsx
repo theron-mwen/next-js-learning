4da0c816bbef84f59f35ae759cde5e22a91ddae2
Theron <theron-mwen>
2026-04-02 23:03:35 +0800

    Tuts 26 - Recovering from Errors

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,4 +1,24 @@
 "use client";
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div> {error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const handleClick = () => {
+    startTransition(() => {
+      reset();
+      router.refresh();
+    });
+  };
+  return (
+    <div>
+      <p> {error.message}</p>
+      <button onClick={handleClick}>Try again</button>
+    </div>
+  );
 }
