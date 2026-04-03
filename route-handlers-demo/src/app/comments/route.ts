7ff8b5c839939c85811be3294e03945351df805d
Theron <theron-mwen>
2026-04-03 19:57:13 +0800

    Tuts 37 - Handling POST Requests

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,9 +1,25 @@
-import {comments} from "./data" 
+import { comments } from "./data";
 
 export async function GET() {
-    return new Response(JSON.stringify(comments), {
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+  return new Response(JSON.stringify(comments), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  const { text } = await request.json();
+  const newComment = {
+    id: String(comments.length + 1),
+    text,
+  };
+  comments.push(newComment);
+
+  return new Response(JSON.stringify(comments), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
 }
