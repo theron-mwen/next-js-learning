41cd5c460111fafa31019dc848320265f4594291
Theron <theron-mwen>
2026-04-03 20:05:09 +0800

    Tuts 39 - Handling PATCH Requests

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,4 +1,5 @@
 import { comments } from "../data";
+
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ id: string }> },
@@ -9,6 +10,28 @@ export async function GET(
     return new Response("Comment not found", { status: 404 });
   }
   return new Response(JSON.stringify(comment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  const commentIndex = comments.findIndex((c) => c.id === id);
+
+  if (commentIndex === -1) {
+    return new Response("Comment not found", { status: 404 });
+  }
+
+  const updatedData = await request.json();
+  const {text} = updatedData;
+  comments[commentIndex].text = text;
+
+  return new Response(JSON.stringify(comments[commentIndex]), {
     headers: {
       "Content-Type": "application/json",
     },
