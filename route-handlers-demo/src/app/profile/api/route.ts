47d50d0a5feacbfb13ac42f9e89f93eb7f99ed37
Theron <theron-mwen>
2026-04-05 00:32:57 +0800

    Tuts 43 - Cookies in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,14 +1,21 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
-  // const requestHeaders = new Headers(request.headers);
-  // console.log(requestHeaders);
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders);
 
   const headerList = await headers();
   console.log(headerList.get("authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log("Theme from cookie:", theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "20");
+  console.log(cookieStore.get("resultPerPage"));
+
   return new Response("<h1>Profile API Data</h1>", {
-    headers: { "Content-Type": "text/html" }
+    headers: { "Content-Type": "text/html", "Set-Cookie": "theme=dark;" },
   });
 }
-
