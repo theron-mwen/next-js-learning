b481bde44fe19fc74e0cbd251d928c83b628cdb3
Theron <theron-mwen>
2026-04-04 20:12:14 +0800

    Tuts 42 - Headers in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {
-  return new Response("Profile API Data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders);
+
+  const headerList = await headers();
+  console.log(headerList.get("authorization"));
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: { "Content-Type": "text/html" }
+  });
 }
+
