137c389b11b97d705720027ecaf4d1c6e8af36d8
Theron <theron-mwen>
2026-04-06 19:37:11 +0800

    Tutorial - 55 - Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,10 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+
+export default async function AboutPage() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log("Current theme:", theme);
   console.log("Rendering AboutPage");
   return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 }
